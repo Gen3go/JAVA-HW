91d6dd728c68d3f3a22b17eee31db5eb898843b5
Bor P <golovanovaea.7@gmail.com>
2023-03-25 11:24:26 +0300

    Second task solved

--- Main.java
+++ Main.java
@@ -3,10 +3,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        FirstTask ();
+        // FirstTask ();
+        SecondTask();
     }
     // Вычислить n-ое треугольного число (сумма чисел от 1 до n)
-        public static void FirstTask (){
+    public static void FirstTask (){
             System.out.println("Write the number: ");
             Scanner scanner = new Scanner(System.in);
             int n = scanner.nextInt();
@@ -20,6 +21,23 @@ public class Main {
             System.out.println(count + m);
             }
     }
+    // Вычислить n! (произведение чисел от 1 до n)
+    public static void SecondTask(){
+        System.out.println("Write the number: ");
+            Scanner scanner = new Scanner(System.in);
+            int n = scanner.nextInt();
+            int m = 1;
+            int k = 1;
+            int g = 2;
+            
+            if(n != 1){
+                while(n > k){
+                    m = m * g;
+                    k += 1;
+                    g += 1;
+                }
+            System.out.println(m);
+            }
+    }
 
-    
 }
