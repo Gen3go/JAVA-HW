33cd64637575d04149303abad07ff0760c4af786
Bor P <golovanovaea.7@gmail.com>
2023-03-25 12:04:43 +0300

    third task solved

--- Main.java
+++ Main.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         // FirstTask ();
-        SecondTask();
+        // SecondTask();
+        ThirdTask();
     }
     // Вычислить n-ое треугольного число (сумма чисел от 1 до n)
     public static void FirstTask (){
@@ -39,5 +40,20 @@ public class Main {
             System.out.println(m);
             }
     }
+    
+    // Вывести все простые числа от 1 до 1000
+    // (числа, которые делятся только на 1 и на себя без остатка)
+    public static void ThirdTask(){
+
+        int n = 1;
+        while(n < 1000){
+            if(n % n == 0);
+            System.out.println(n);
+            n += 1;
+        }
+    }
+
+
+
 
 }
