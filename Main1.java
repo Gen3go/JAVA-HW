c83482d6b9a14aa1c93f258ad4c9a0291f15ba1f
Bor P <golovanovaea.7@gmail.com>
2023-04-01 13:27:56 +0300

    Не решена

--- Main1.java
+++ Main1.java
@@ -2,10 +2,11 @@ public class Main1 {
     public static void main(String[] args) {  
         
         String JsonString = "{\"фамилия\":\"Иванов\",\"оценка\":\"5\",\"предмет\":\"Математика\"},{\"фамилия\":\"Петрова\",\"оценка\":\"4\",\"предмет\":\"Информатика\"},{\"фамилия\":\"Краснов\",\"оценка\":\"5\",\"предмет\":\"Физика\"}";
-
+        String student = null;
         System.out.println(parseString (JsonString));
+        String filePath = "test1.txt";
     } 
-    public static String parseString (){
+    public static String parseString (String JsonString, String student){
         /* 
         Дана json строка { { "фамилия":"Иванов","оценка":"5","предмет":"Математика"},
         {"фамилия":"Петрова","оценка":"4","предмет":"Информатика"},
@@ -16,9 +17,25 @@ public class Main1 {
         Студент Краснов получил 5 по предмету Физика. 
         Используйте StringBuilder для подготовки ответа
         */
-        
-        
-        return null;
+        StringBuilder stringBuilder = new StringBuilder();
+
+        if()
+
+        student = stringBuilder.append();
+
+        return student;
+
+    }
+    static void writeToFile(String s1, String filePath){
+        FileWriter writer = null;
+        try{
+        writer = new FileWriter(filePath, false);
+        writer.write();
+        writer.flush();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
 
     }
 }
