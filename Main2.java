635be024d2afde1759ccabc9b3accc3b518505db
Bor P <golovanovaea.7@gmail.com>
2023-04-05 20:00:21 +0300

    Половина работает

--- Main2.java
+++ Main2.java
@@ -7,25 +7,25 @@ public class Main2 {
         /*
          * Пусть дан произвольный список целых чисел, удалить из него четные числа/
          */
-        List<Integer> arr = new ArrayList<>();
-        List<Integer> arr2 = new ArrayList<>();
-        createArray(arr);
-        System.out.println(arr);
-        // sortArray(arr);
-        // System.out.println(createArray());
+        
+       
+        System.out.println(createArray());
+        sortArray(arr);
+
     }
 
-    static int createArray(List<Integer> arr) {
+    static List<Integer> createArray() {
+        List<Integer> arr = new ArrayList<>();
         Random rnd = new Random();
         for (int i = 0; i < 10; i++) {
             arr.add(rnd.nextInt(2, 10));
         }
-        System.out.println(arr);
 
         return arr;
     }
 
-    static void sortArray(List<Integer> arr, List<Integer> arr2) {
+    static List<Integer> sortArray(List<Integer> arr) {
+        List<Integer> arr2 = new ArrayList<>();
         for (int i = 0; i < arr.size(); i++) {
             if (arr[i] % 2 == 0) {
                 arr2.add(arr[i]);
