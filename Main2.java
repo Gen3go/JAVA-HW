acb84fb53b62de91888113030847106c168b4575
Bor P <golovanovaea.7@gmail.com>
2023-04-05 18:28:27 +0300

    Не решена

--- Main2.java
+++ Main2.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class Main2 {
@@ -7,22 +8,31 @@ public class Main2 {
          * Пусть дан произвольный список целых чисел, удалить из него четные числа/
          */
         List<Integer> arr = new ArrayList<>();
-        createArray();
-        sortArray(arr);
+        List<Integer> arr2 = new ArrayList<>();
+        createArray(arr);
+        System.out.println(arr);
+        // sortArray(arr);
+        // System.out.println(createArray());
     }
 
-    static void createArray() {
-
+    static int createArray(List<Integer> arr) {
         Random rnd = new Random();
         for (int i = 0; i < 10; i++) {
-            arr.add(rnd.nextInt(10));
+            arr.add(rnd.nextInt(2, 10));
         }
+        System.out.println(arr);
+
+        return arr;
     }
 
-    static void sortArray(int arr) {
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] % 2 == 0)
-                arr.add(arr[i]);
+    static void sortArray(List<Integer> arr, List<Integer> arr2) {
+        for (int i = 0; i < arr.size(); i++) {
+            if (arr[i] % 2 == 0) {
+                arr2.add(arr[i]);
+            }
+            System.out.println(arr2);
         }
+
     }
+
 }
